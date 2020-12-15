bcf27c37ad1449866c942e85411cd0931c2264bb
Diego <diego@archlinux.localdomain>
2020-12-15 03:44:24 -0500

    server updated

--- server/server.py
+++ server/server.py
@@ -12,28 +12,13 @@ CORS(app)
 
 @app.route('/query', methods=["POST"])
 def query():
-  """ if request.files:
-    image = request.files['image']
-    image.save(app.config['UPLOAD_FOLDER'], image.filename)
-    print("img saved")
-    return redirect(request.url)
-  return render_template("public/upload_image.html") """
-
-  print(request.files)
   file = request.files['file']
-  print(request.form)
   k = json.loads(request.form['data'])['k']
-  print('debug2')
   if file.filename != '':
-    print('debug3')
     filename = secure_filename(file.filename)
-    print('debug4')
   file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-  print('debug5')
   q = r_tree.get_img_vector(file)
-  print('debug6')
   lres = index.priority_knn(q, k)
-  print('debug7')
   return Response(json.dumps(lres), status = 202, mimetype="application/json")
 
 @app.route('/', methods=["GET"])
